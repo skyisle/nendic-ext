7202c5414fec8493f12a2c9770ed9d2fc83e0b7c
skyisle <skyisle@gmail.com>
2012-04-18 14:39:23 +0900

    fd 단축기 삭제

--- src/content/event.shortcut.js
+++ src/content/event.shortcut.js
@@ -5,12 +5,6 @@ J.module('event.shortcut', {
 
   $ps: null,
     
-  _doing: false,
-    
-  _timeLimit: 1000,
-    
-  _timer: null,
-    
   init: function () {
     this.bindKeyEvent();
   },
@@ -22,8 +16,6 @@ J.module('event.shortcut', {
     // keydown일 경우, 한글일 때 키코드를 정확하게 찾지 못하는 버그가 있다.
     // keyup 이벤트를 할당한다.
     document.documentElement.addEventListener('keyup', function (evt) {
-      t.process(evt.keyCode);
-      
       switch (evt.keyCode) {
                     
       case 67: // c
@@ -55,47 +47,5 @@ J.module('event.shortcut', {
       }
             
     });
-  },
-    
-  /**
-   * 텍스트를 선택하고 fd 키를 연속(1초 이내)으로 클릭하면,
-   * 검색을 요청하는 메세지를 보낸다.
-   * (fd = find dictionary)
-   * @param {number} keyCode
-   */
-  process: function (keyCode) {
-    var t = this;
-    if (this._doing === false && keyCode === 70) { // 최초에 f 키를 누른다.
-      this._doing = true;
-            
-      this._timer = setTimeout(function () {
-        t._doing = false;
-        t._timer = null;
-      }, this._timeLimit);
-    } else if (this._doing === true) {
-      if(keyCode === 68) { // 연속으로 d 키를 누른다. d가 아니면 정리한다.
-        var selectionText = this.getSelectedText();
-        if (selectionText) {
-          this.$ps.publish('shortcut.search', {
-            when: 'all',
-            value: selectionText
-          });
-        }
-      }
-      
-      this._doing = false;
-      if (this._timer) {
-        clearTimeout(this._timer);
-        this._timer = null;
-      }
-    }
-  },
-  
-  /**
-   * 현재 선택한 문자열을 가져온다.
-   * @return {string}
-   */
-  getSelectedText: function () {
-    return window.getSelection().toString().trim();
   }
 });
